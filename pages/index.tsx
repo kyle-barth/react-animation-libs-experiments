389c5ffe727a902de89c5bbf48ef946f7ed9f208
Kyle Barth <kylebarth@me.com>
2022-07-31 17:36:07 +0100

    basic react-spring impl + fix overflow bug caused by sidenav's

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,26 @@
+import styled from "@emotion/styled";
 import type { NextPage } from "next";
 import CSSNavBar from "../components/css-navbar";
 import FramerMotionNavBar from "../components/framermotion-navbar";
+import ReactSpringNavBar from "../components/reactspring-navbar";
 import ReactTransitionGroupNavBar from "../components/reacttransitiongroup-navbar";
 
 const Home: NextPage = () => {
   return (
-    <>
+    <Container>
       <CSSNavBar />
       <FramerMotionNavBar />
       <ReactTransitionGroupNavBar />
-    </>
+      <ReactSpringNavBar />
+    </Container>
   );
 };
 
 export default Home;
+
+const Container = styled.div`
+  position: absolute;
+  overflow-x: hidden;
+  width: 100%;
+  height: 100%;
+`;
